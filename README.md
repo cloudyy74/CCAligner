a77fe40e7c8b04d4cc5e1204abaf2975b401f17c
lokiplot <plotnikofften@gmail.com>
2023-08-09 16:14:42 +0300

    now it is ready to use "from the box"

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-#CCAligner
+# CCAligner
 
 This repo contains an implementation of CCAligner algorithm for script written on Python.
 
-It is required to run ``git clone https://github.com/tree-sitter/tree-sitter-python`` in the project before working.
+It is required to run 
+
+``git clone https://github.com/tree-sitter/tree-sitter-python``
+
+``git clone https://github.com/tree-sitter/tree-sitter-java``
+
+in the project root before working.
+
 
 Example:
 
-``python3 ./src/ccaligner.py -from ./data/original_codebases/c
-odebase1``
-
+``python3 ./src/ccaligner.py -from ./data/original_codebases/paper_example -l java``
 
--- src/ccaligner.py
+++ src/ccaligner.py
@@ -19,9 +19,9 @@ pretty_loc = "./data/normalized_codebases/"
 if os.path.exists(pretty_loc):
     rmtree(pretty_loc)
 
-if (language == 'python'):
+if language == 'python':
     pp = prpr.PrettyPrinterPy(codebase_loc, pretty_loc, language)
-elif (language == 'java'):
+elif language == 'java':
     pp = prpr.PrettyPrinterJava(codebase_loc, pretty_loc, language)
 
 pp.pretty_print()
@@ -36,4 +36,5 @@ for file1, file2 in pairs:
     file_name2 = file2.split('/')[-2] + '.java'
     fragment1 = file1.split('/')[-1][:-5]
     fragment2 = file2.split('/')[-1][:-5]
-    print(f"{file_name1} and {file_name2} contain codeclone in lines {fragment1} and {fragment2} respectively")
+    if file_name1 != file_name2:
+        print(f"{file_name1} and {file_name2} contain codeclone in lines {fragment1} and {fragment2} respectively")
