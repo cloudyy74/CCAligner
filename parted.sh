14c2a5636c568055fcc028c07504d7ea3722bf75
lokiplot <plotnikofften@gmail.com>
2023-08-28 14:01:02 +0000

    right script for partitial launch

--- parted.sh
+++ parted.sh
@@ -2,7 +2,7 @@
 date
 
 
-for dir_number in {5..15} {17..45} {2..3}; do
+for dir_number in 5 38 11 15 21 12 8 18 20 36 26 44 17 22 32 28 45 13 25 29 33 40 19 24 31 37 6 39 34 23 42 43 9 27 41 35 10 7 30 14 3 2 4 ; do
 
         python3 src/ccaligner.py -from ~/benchmark/BigCloneEval/ijadataset/bcb_reduced/"$dir_number" -l java -theta 60
 
@@ -10,11 +10,13 @@ for dir_number in {5..15} {17..45} {2..3}; do
 
           cat clones.csv >> all_clones.csv
         else
-          cp clones all_clones.csv
+          cp clones.csv all_clones.csv
         fi
-        cp clones "clones${dir_number}"
+        cp clones.csv "clones${dir_number}"
 
-        echo "$dir_number"
+        echo "$dir_number" >> log_4
         date
+	echo "$dir_number"
 done
 date
+
