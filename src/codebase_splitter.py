e7a2c11e4cba7f86584dc00d4f255f729f6b78b0
lokiplot <plotnikofften@gmail.com>
2023-10-09 16:36:44 +0300

    change in number of parts

--- src/codebase_splitter.py
+++ src/codebase_splitter.py
@@ -14,7 +14,7 @@ for file in os.listdir(dir):
     if os.path.isdir(d):
         list_of_big_dirs.append(d)
 
-for i in range(8):
+for i in range(number_of_parts):
     if not os.path.exists(os.path.join(dir_dest, str(i))):
         os.mkdir(os.path.join(dir_dest, str(i)))
 
@@ -23,9 +23,9 @@ for big_dir in list_of_big_dirs:
     for small_dir in os.listdir(big_dir):
         modulo = 0
         if small_dir.isnumeric():
-            modulo = int(small_dir) % 8
+            modulo = int(small_dir) % number_of_parts
         else:
-            modulo = ord(small_dir[-1]) % 8
+            modulo = ord(small_dir[-1]) % number_of_parts
         if not os.path.exists(os.path.join(*[dir_dest, str(modulo), big_dir.split('/')[-1]])):
             os.mkdir(os.path.join(*[dir_dest, str(modulo), big_dir.split('/')[-1]]))
         copytree(os.path.join(big_dir, small_dir), os.path.join(*[dir_dest, str(modulo), big_dir.split('/')[-1], small_dir]))
