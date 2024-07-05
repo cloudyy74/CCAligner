f9ce3111b36d90a34c13148051cd50f3e9aee242
lokiplot <plotnikofften@gmail.com>
2024-07-05 17:09:17 +0300

    corrected comments for structures

--- src/clone_detection/algorithm.py
+++ src/clone_detection/algorithm.py
@@ -27,9 +27,10 @@ class CCalignerAlgorithm:
         self.lang_ext = lang_ext
         for file in glob.glob(self.dir + "/**/*" + lang_ext, recursive=True):
             self.files.append(file)
-        self.cand_map = dict()  # for hash of q-e-grams stores list of blocks, containing it
-        self.lines_in = dict()  # for block
-        self.hash_set = dict()  #
+        self.cand_map = dict()  # for hash of q-e-grams stores dict of blocks, and for each block stores number that
+        # this hash gram has occurred
+        self.lines_in = dict()  # for code block stores number of lines
+        self.hash_set = dict()
         self.cand_pair = dict()  # we store in cand_pair pairs of fragments as keys, and values are cardinality of
         # hashes_intersection
         self.cand_pair_list = list()
